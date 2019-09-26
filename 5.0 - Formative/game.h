d8e17ca0364fed5a84e537e8cddaef4ce18afb18
William PEPIN <williamp10022000@gmail.com>
2019-09-26 15:30:31 +0200

    jeu sans sécu

--- 5.0 - Formative/game.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-void current_round(int round);
-void win(int userScore, bool& isRunning);
-void lose(int computerScore, bool& isRunning);
-void versus(int userInput, int computerInput, int& userScore, int& computerScore, int& round);
-void score(int userScore, int computerScore);
--- 5.0 - Formative/player.cpp
+++
@@ -1,9 +0,0 @@
-#include "player.h"
-
-int user_choice()
-{
-	int userInput = 4;
-	std::cout << "1 - Rock\n2 - Paper\n3 - Scissors\n\nMake your choice: ";
-	std::cin >> userInput;
-	return userInput;
-}
